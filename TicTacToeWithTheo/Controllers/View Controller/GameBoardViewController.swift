5dee2d66e6b035faa90b185573973b7db491885c
Colby Harris <griff33@gmail.com>
2020-04-08 15:44:47 -0600

    Started viewController, added outlets and action

---
+++ TicTacToeWithTheo/Controllers/View Controller/GameBoardViewController.swift
@@ -0,0 +1,41 @@
+//
+//  GameBoardViewController.swift
+//  TicTacToeWithTheo
+//
+//  Created by Colby Harris on 4/8/20.
+//  Copyright © 2020 Colby_Harris. All rights reserved.
+//
+
+import UIKit
+
+class GameBoardViewController: UIViewController {
+
+    //MARK: - Outlets
+    @IBOutlet weak var buttonOne: UIButton!
+    @IBOutlet weak var buttonTwo: UIButton!
+    @IBOutlet weak var buttonThree: UIButton!
+    @IBOutlet weak var buttonFour: UIButton!
+    @IBOutlet weak var buttonFive: UIButton!
+    @IBOutlet weak var buttonSix: UIButton!
+    @IBOutlet weak var buttonSeven: UIButton!
+    @IBOutlet weak var buttonEight: UIButton!
+    @IBOutlet weak var buttonNine: UIButton!
+    
+    
+    
+    //MARK: - Properties
+    
+    //MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    //MARK: - Actions
+    @IBAction func buttonTapped(_ sender: Any) {
+    }
+    
+    
+    //MARK: - HelperFunc's
+
+}
